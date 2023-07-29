3e178f5abe1e87bdf3b803f38e7fa4e1febec32d
Rahul-Sahani04 <devilken43@gmail.com>
2023-07-29 20:20:56 +0530

    BRRRR

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const search = require('./routes/getRandom');
 const users = require('./routes/ShowData');
-const add = require('./routes/add')
-const deleteCookie = require('./routes/delete')
+const add = require('./routes/add');
+const deleteUser = require('./routes/delete');
 
-port = 8000
-const app = express()
+const app = express();
 app.use(express.json());
+
 app.get('/', (request, response) => {
-    response.json({
-        info: 'Node.js, Express, and Postgres API'
-    })
-})
-app.get('/cookie', users.ShowData)
+  response.json({
+    info: 'Node.js, Express, and Postgres API',
+  });
+});
+
+app.get('/cookie', users.ShowData);
 app.get('/cookie/random', search.getRandom);
-app.post('/cookie/add', add.addCookie)
-app.delete('/cookie/:id', deleteCookie.deleteCookie)
-app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
+app.post('/cookie/add', add.addCookie);
+app.delete('/cookie/:id', deleteUser.deleteCookie);
+
+module.exports = {app};
