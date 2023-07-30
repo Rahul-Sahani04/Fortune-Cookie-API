2f67d4fab7c844f688209bb0d07c8cb30c0f9166
Rahul-Sahani04 <devilken43@gmail.com>
2023-07-30 12:28:46 +0530

    Fixes

--- routes/getRandom.js
+++ routes/getRandom.js
@@ -1,24 +1,13 @@
 const { pool } = require('../pool');
-// const express = require('express');
-// const router = express.Router();
 const getRandom = (request, response) => {
     
     pool.query('SELECT * FROM fortune_messages ORDER BY random() LIMIT 1', (error, results) => {
         if (error) {
             throw error;
         }
-        // var postData = results.rows;
-        // console.log(postData);
-        
         response.send(results.rows);
     });
 };
-
-
-// router.get("/cookie/random", (req, res) => {
-//     console.log("SENDING")
-//     res.send(postData);
-//   });
 
 module.exports = {
     getRandom,
